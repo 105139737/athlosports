f5707490ceaff5db3fd285c79806bb5cbe44f0b3
Jafar <ikbal.jafar46@gmail.com>
2024-06-23 15:30:12 +0530

    wip

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include "header.php";
 			<h1><?=$comp_nm;?><small>Control panel</small></h1>
 			<ol class="breadcrumb">
 				<li><a href="#"><i class="fa fa-home"></i> Home</a></li>
-				<li class="active">Dashboard</li>
+				<li class="active"> Dashboard </li>
 			</ol>
 		</section>
 
